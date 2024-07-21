9421ba0f18170bd974ee904f1fea412a29c3c26f
Tiare Infante <tiare.infante.snmartin@gmail.com>
2024-07-21 17:31:12 -0400

    Added GET /api/taxis/{id}/locations endpoint with tests and documentation

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const { swaggerDocs } = require('./src/routes/swagger.js');
 const dotenv = require('dotenv');
-const { router: taxisRouter } = require('./src/routes/taxisRoutes.js');
+const taxisRouter = require('./src/routes/taxisRoutes.js');
 
-dotenv.config; // para cargar las variables
+dotenv.config(); // para cargar las variables
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,11 +13,12 @@ app.use(express.json());
 
 // Enrutador para lasrutas
 app.use('/api/taxis', taxisRouter);
+
 // Config de Swagger
 swaggerDocs(app, PORT);
 
 app.listen(PORT, () => {
-  // console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
 
 module.exports = app;
--- src/controllers/taxisController.js
+++ src/controllers/taxisController.js
@@ -33,4 +33,44 @@ module.exports = {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   },
+
+  getTaxiLocations: async (req, res) => {
+    const taxiId = req.params.id; // Obtener el id desdo los parámetros
+    const date = req.query.date; // Obtener la fecha de los parámetros de consulta.
+    // Obtener los parámetros de la consulta page y limit
+    const limit = parseInt(req.query.limit, 10);
+    const page = parseInt(req.query.page, 10);
+
+    console.log(`limit: ${limit}, page: ${page}`);
+
+    // Verficar que limit u offset sean números válidos
+    if (isNaN(limit) || isNaN(page)) {
+      console.log('Invalid query parameters detected');
+      return res.status(400).json({ error: 'Invalid query parameters' });
+    }
+
+    const offset = (page - 1) * limit;
+    console.log(`offset: ${offset}`);
+
+    try {
+      // Consultar a la DB para obtener las ubicaciones.
+      const query = `
+      SELECT date, latitude, longitude
+      FROM trajectories
+      WHERE taxi_id = $1 AND DATE(date) = $2
+      ORDER BY date
+      LIMIT $3 OFFSET $4
+      `;
+      const result = await db.pool.query(query, [taxiId, date, limit, offset]);
+      if (result.rows.length === 0) {
+        return res
+          .status(404)
+          .json({ error: 'No locations found for the given taxi ID and date' });
+      }
+      res.status(200).json(result.rows);
+    } catch (error) {
+      console.error('Error getting taxi locations:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
 };
